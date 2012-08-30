d2c2c07b2e03e5c067d3b7056a882c3c54fda1e8
Andy <andypro@vt.edu>
2012-08-30 04:54:04 -0500

    Created/enabled point edit view.

--- client/points.js
+++ client/points.js
@@ -1,5 +1,55 @@
 //  Requires jQuery and knockoutJs
 
 $(document).ready(function() {
-	
+	function Point(data) {
+		this.latitude  = ko.observable(data.latitude);
+		this.longitude = ko.observable(data.longitude);
+		this.dateTime  = ko.observable(data.dateTime);
+	}
+
+	function PointListViewModel() {
+		//  Data
+		var self          = this;
+		self.points       = ko.observableArray([]);
+		self.newLatitude  = ko.observable();
+		self.newLongitude = ko.observable();
+		self.newDateTime  = ko.observable();
+
+		self.newPointsCount = ko.computed(function() {
+			return ko.utils.arrayFilter(self.points(), function(point) { return !point._destroy; });
+		});
+
+		//  Operations
+		self.addPoint = function() {
+			self.points.push(new Point({
+				latitude : this.newLatitude(),
+				longitude: this.newLongitude(),
+				dateTime : this.newDateTime()
+			}));
+		};
+
+		self.removePoint = function(point) { self.points.destroy(point); };
+
+		self.save = function() {
+			$.ajax("/map/edit", {
+				data   : ko.toJSON({ points: self.points }),
+				type   : "post", contentType: "application/json",
+				success: function(result) { alert(result); }
+			});
+		};
+
+		//  Load initial state from server, convert it to Point instances, then populate self.points
+		$.getJSON("/map/json", function(data, textStatus, jqXHR) {
+			var mappedPoints = $.map(data.response.feedMessageResponse.messages.message, function(item) {
+				return new Point(item);
+			});
+
+			self.points(mappedPoints);
+
+			$('body').fadeIn('fast');
+		});
+	}
+
+	//  One call - that's all!
+	ko.applyBindings(new PointListViewModel());
 });
--- server/servepoints.js
+++ server/servepoints.js
@@ -1,14 +1,67 @@
-var http = require('http'),
-	fs   = require('fs'),
-	url  = require('url');
+var http  = require('http'),
+	fs    = require('fs'),
+	url   = require('url');
 
 var server = http.createServer(function(req, res) {
-	var qs =   url.parse(req.url, true).query,
+	var qs   = url.parse(req.url, true).query,
 		path = url.parse(req.url, true).path;
 
 	if(path === '/map/edit') {
 		if(req.method === 'POST') {
-			res.end('POSTed!');
+			var postBody = [];
+
+			req.on('data', function(chunk) {
+				postBody.push(chunk.toString());
+			});
+
+			req.on('end', function() {
+				var jsonBody = JSON.parse(postBody);
+
+
+
+				var jsonMaster = {
+					response: {
+						feedMessageResponse: {
+							/*count: jsonBody.points.length,
+							feed: {
+								id           : '1oB6iVTMYgA9WcUjL8eFSDIoGqQGrAZ3B',
+								link         : 'https://share.findmespot.com/shared/faces/viewspots.jsp?glId=1oB6iVTMYgA9WcUjL8eFSDIoGqQGrAZ3B',
+								usage        : '',
+								status       : 'ACTIVE',
+								linkMode     : 'PRIVATE',
+								linkName     : 'Test for API',
+								linkDesc     : 'Test for API',
+								showCustomMsg: 'Y',
+								daysRange    : 7
+							},
+							totalCount   : 1,
+							activityCount: 1,*/
+							messages: {
+								message: []
+							}
+						}
+					}
+				};
+
+				for(var i=0; i < jsonBody.points.length; ++i) {
+					jsonMaster.response.feedMessageResponse.messages.message.push({
+						createDate   : new Date(),
+						dateTime     : jsonBody.points[i].dateTime,
+						id           : '',
+						latitude     : jsonBody.points[i].latitude,
+						longitude    : jsonBody.points[i].longitude,
+						messageDetail: '',
+						messengerId  : '',
+						messengerName: '',
+						showCustomMsg: '',
+						timeInSec    : '',
+						type         : ''
+					});
+				}
+
+				fs.writeFileSync('/home/ap/dev/www/spot/hi.txt', JSON.stringify(jsonMaster));
+				res.end('Update successful: ' + postBody);
+			});
 		}
 		else {
 			res.writeHead(200);
@@ -22,16 +75,22 @@ var server = http.createServer(function(req, res) {
 			'Content-Type': 'application/javascript'
 		});
 
-		if(!qs.callback) {
-			res.end('\'You must specify a callback parameter.\' ' + path);
-			return;
-		}
-		
 		fs.exists('/home/ap/dev/www/spot/spotJson.txt', function(exists) {
 			if(exists) {
 				fs.readFile('/home/ap/dev/www/spot/spotJson.txt', 'utf8', function(err, data) {
-					//  Format json file into a json-p response
-					res.end(qs.callback + '(' + data + ')');
+					if(path === '/map/json') {
+						res.end(data);
+					}
+					else {
+						if(!qs.callback) {
+							res.end('\'You must specify a callback parameter.\' ' + path);
+							return;
+						}
+						else {
+							//  Format json file into a json-p response
+							res.end(qs.callback + '(' + data + ')');
+						}
+					}
 				});
 			}
 			else {
